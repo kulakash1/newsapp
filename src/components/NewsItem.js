93282ddd7aa69bf8c595ba8f98ca3751e89b3ff2
Akash Kulshrestha <akashkulshrestha7@gmail.com>
2024-03-09 03:30:10 +0530

    added key, fs error still persists -> commented the code

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,9 +6,9 @@ export default function NewsItem(props) {
       <div className="card" style={{width: "18rem"}}>
   <img src={props.imageUrl} className="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">{props.description}</p>
-    <a href="/newsDetail" className="btn btn-primary">Read More</a>
+    <h5 className="card-title">{props.title}...</h5>
+    <p className="card-text">{props.description}...</p>
+    <a href={props.newsUrl} target="_blank" className="btn btn-primary">Read More</a>
   </div>
 </div>
     </>
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import NewsItem from "../components/NewsItem";
-import path from "path";
-import fs from "fs";
+// import path from "path";
+// import fs from "fs";
+
 // import fs from 'fs-extra'
 // const fs = require('fs');
 
 export default function News() {
   const [articles,setArticles] = useState(null);
+
   useEffect(() => {
     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=bd3c695c98ce410a8e58f0185bf5a92a&pageSize=100')
     .then(res => {
@@ -15,14 +17,14 @@ export default function News() {
     })
     .then(data => {
       
-      const filePath = path.join('./src/data', 'sampleOutput.json');
-      fs.writeFile(filePath, JSON.stringify(data.articles), (err) => {
-        if (err) {
-          console.error('Error writing to file:', err);
-        } else {
-          console.log('Data written to sampleOutput.json');
-        }
-      });
+      // const filePath = path.join('./src/data', 'sampleOutput.json');
+      // fs.writeFile(filePath, JSON.stringify(data.articles), (err) => {
+      //   if (err) {
+      //     console.error('Error writing to file:', err);
+      //   } else {
+      //     console.log('Data written to sampleOutput.json');
+      //   }
+      // });
 
       setArticles(data.articles);
       console.log(data);
@@ -39,29 +41,20 @@ export default function News() {
       <div className="row">
       {articles.map((article) => {
       return (
-        <div className="col-md-4">
-        <NewsItem title={article.title} description={article.description} imageUrl={article.urlToImage} />
+        <div className="col-md-4" key={article.url}>
+        {/* <NewsItem title={article.title} description={article.description} imageUrl={article.urlToImage} /> */}
+        <NewsItem
+  title={article.title ? article.title.slice(0, 40) : ""}
+  description={article.description ? article.description.slice(0, 80) : ""}
+  imageUrl={article.urlToImage || ""}
+  newsUrl={article.url || ""}
+/>
       </div>
       );
 
       })}
       </div>
       }
-
-      {/* <div className="row">
-        <div className="col-md-4">
-          <NewsItem title="myTitle" description="Test" imageUrl="" />
-        </div>
-        <div className="col-md-4">
-          <NewsItem title="myTitle2" description="Test2" imageUrl="" />
-        </div>
-        <div className="col-md-4">
-          <NewsItem title="myTitle3" description="Test3" imageUrl="" />
-        </div>
-        <div className="col-md-4">
-          <NewsItem title="myTitle4" description="Test4" imageUrl="" />
-        </div>
-      </div> */}
     </div>
   );
 }
